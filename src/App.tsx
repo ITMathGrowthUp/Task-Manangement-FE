e554dfa8f48fc9354e00746556bc2bfc7ef5fe53
hailuu-local-devices <hoanghai.luu.71@gmail.com>
2025-03-22 16:42:47 +0700

    trigger build

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
-          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
