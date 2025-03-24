2d5162f6269c24936516e6a928cd47b0a28810f5
hailuu-local-devices <hoanghai.luu.71@gmail.com>
2025-03-24 22:45:26 +0700

    ci: configure Vite server with strict port and allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
   server: {
     port: 3000,
     host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: ["task-manangement-fe.onrender.com"]
   },
   css: {
     devSourcemap: true
