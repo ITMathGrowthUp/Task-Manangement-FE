2ae7698e39e65f537453f12027cb3c651d8bc74d
hailuu-local-devices <hoanghai.luu.71@gmail.com>
2025-03-29 17:04:42 +0700

    test: erorr

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
-      </Routes>
+      {/*</Routes>*/}
     </>
   )
 }
